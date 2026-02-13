62b7d5d83d245f1b9228761182434fb28709a6b3
vijeth44-pro <captainmncaptain@gmail.com>
2026-02-13 06:22:30 +0530

    Initial commit in index

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import express from 'express'
 import mongoConnection from './Db.js'
+import userRoutes from "./routes/userRoutes.js"
+import studentRoutes from "./routes/studentRoutes.js"
 
 const app = express()
 app.use(express.json())
+// mildware for frontend
+// means bridge
 mongoConnection()
 
-const PORT = 7000
+const PORT = 1000
 
-//test api(optional)
-app.get("/test",(req,res)=>{
-    res.send("Hi iam backend!")
+// test api(optional)
+app.get("/test",(req, res)=>{
+     res.send("Hi iam backend!")
 })
 
-app.listen(PORT, () =>{
-    console.log("Hi !,Backend running on "+PORT)
+app.listen(PORT, ()=>{
+    console.log("Hi !,Iam Backend running on "+PORT)
 })
+
+// call
+// mildware 
+// means bridge
+// app.use path filename
+app.use("/users",userRoutes)
+app.use("/students",studentRoutes)
+
+
+// http://localhost:1000/users/addUser complete api  base path till port no ,mildware,routing path  endpoint
