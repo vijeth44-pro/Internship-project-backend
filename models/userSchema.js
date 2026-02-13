a48c1acf22b3d250c0e8f0884e769d12b8ea350d
vijeth44-pro <captainmncaptain@gmail.com>
2026-02-13 06:23:02 +0530

    created user schema

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,35 +1,25 @@
-// blueprint or structure of data
-// entire thing inside of object   is schema
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     name : {
         type : String,
         required : true
     },
-    email : {
+    email: {
         type : String,
         required : true
     },
-    phone : {
+    phone:{
         type : String,
         required : true
     }
-})
+    })
 
-// entire thing will be assigned in model user variable 
-// user is the collection name in mongo db
-// automatically adds s in mongo db
-const User = mongoose.model("User",userSchema)
+    const User = mongoose.model('User', userSchema);
 export default User;
 
-// steps
-// 1.import mongoose
-// 2.create a variable & assign new mongoose.Schema({object})
-// 3.inside object create all the keys & for those keys assign one object consist of blueprint of data
-// 4.create one variable & assign mongoose .model("collectionName",schemaVariable)const User = mongoose.model("User",userSchema)
-// 5.export the varable
-
-
-// task product category schema
-//
+//step 1: import mongoose
+//step 2: create a schema using mongoose.Schema
+//step 3: inside the schema, define the fields and their types and whether they are required or not.
+//step 4: create one variable and assign it to mongoose.model,("collection name", schema name)
+//step 5: export the variable using export default variable name.
