a586830776a64da65dbb92fa8a5b1427134fd86f
vijeth44-pro <captainmncaptain@gmail.com>
2026-02-13 06:22:06 +0530

    Initial commit in Db

--- Db.js
+++ Db.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
+const mongo_url ='mongodb://localhost:27017/Diploma'
+// install mongoes in terminal (npm install mongoose)
+// connection string for connect server & database
+// connection string + datbase name (path from mongodb /diploma) doesn't give space instead give  _ -
 
-const mongo_Url = 'mongodb://localhost:27017/lord';
+// for exception handling try catch use  try excute code ctach for error
+// async is used for feature time,await async use madidre awit use madbodu
+// 
 
-const mongoConnection = async() => {
+const mongoConnection = async() =>{
   try {
-    await mongoose.connect(mongo_Url)
-    console.log("Database connected successfully")
+    await mongoose.connect(mongo_url)
+    console.log("database connected successfully")
   } catch (error) {
-    console.log("Database connection failed");
+    console.log("error in connecting database")
   }
 }
-
 export default mongoConnection;
