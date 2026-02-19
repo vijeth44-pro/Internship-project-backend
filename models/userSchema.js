efd953c079a0ef3fdcdde203d2e8b0a9cca7b474
vijethrai-py <vijethrai48@gmail.com>
2026-02-19 15:54:38 +0530

    fix(userSchema): add missing password field to user schema

--- models/userSchema.js
+++ models/userSchema.js
@@ -12,6 +12,10 @@ const userSchema = new mongoose.Schema({
     phone:{
         type : String,
         required : true
+    },
+    password:{
+        type: String,
+        required : true
     }
     })
 
