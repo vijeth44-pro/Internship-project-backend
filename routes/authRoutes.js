9cd5f84ff471484104a69fd5d20d4bf8227c4d69
vijethrai-py <vijethrai48@gmail.com>
2026-02-19 20:01:45 +0530

    add(authRoutes): include login route in authentication routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { registerUser } from "../controller/authController.js";
+import { registerUser,loginUser } from "../controller/authController.js";
 
 const router = express.Router()
 
 router.post("/register", registerUser)
+router.post("/login", loginUser);
 
 export default router;
