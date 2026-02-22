cae13672fdd8c1dbe045d8b4bb7c90a2438f8e22
vijethrai-py <vijethrai48@gmail.com>
2026-02-22 21:57:20 +0530

    add(authUser): implement middleware for JWT authentication

---
+++ middleware/authUser.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken'
+
+const SECRETKEY = 'MySecurityKey'
+
+ const  authUser = async(req,res,next) =>{
+  try {
+    // access token from api request
+    // frontend inda access madudu
+    const token = req.header("auth-token")
+    if(!token){
+        return req.status(401).json({
+            success:false,
+            message:"Token required"
+        })
+    }
+     
+    // decoding the token & verifing it with secret key
+    // reomove secretkey only payload
+    const decoded = await jwt.verify(token,SECRETKEY)
+    // attaching the decoded user data to the request object for further use
+    req.user = decoded 
+    next()
+  } catch (error) {
+    res.status(400).json()
+    success:false
+    message :"Token needed"
+  }
+ }
+ export default authUser
