bd396ce8c6c5350e290596256cc637bae08819e0
vijethrai-py <vijethrai48@gmail.com>
2026-02-28 22:02:48 +0530

    implemented middle ware in routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,8 +3,9 @@
 // export default router;
 import express from 'express';
 import {createPost } from '../controller/postController.js';
+import authUser from '../middleware/authUser.js'
 
 const router  = express.Router();
-router.post("/create",createPost)
+router.post("/create",authUser,createPost)
 
 export default router;
